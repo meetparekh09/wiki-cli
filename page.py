e87694257fa397ff169f2c02edb7ef6139e58231
Meet Parekh <meetparekh@Meets-MacBook-Pro.local>
2018-05-16 11:35:43 -0400

    Prototype Version 0.0.1

--- page.py
+++ page.py
@@ -4,9 +4,12 @@ from main import http
 
 class Page:
     """Holds data section for wikipedia page."""
-    def __init__(self, url):
-        self.page = http.request('GET', url)
-        self.parser = BeautifulSoup(self.page.data, 'html.parser')
+    def __init__(self, page):
+        if isinstance(page, str):
+            page = http.request('GET', page)
+            self.parser = BeautifulSoup(page.data, 'html.parser')
+        else:
+            self.parser = page
 
     def printText(self):
         print(self.parser.text)
