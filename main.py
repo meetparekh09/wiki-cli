773bd490815c22f4031a27d88f00b1231bf00a30
Meet Parekh <meetparekh09@gmail.com>
2018-05-16 11:35:43 -0400

    Prototype Version 0.0.1

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import urllib3
 from bs4 import BeautifulSoup
 from page import *
 
+urllib3.disable_warnings() #temporary fix
 http = urllib3.PoolManager()
 
 if __name__ == '__main__':
-    Pg = Page('https://en.wikipedia.org/wiki/Taj_Mahal')
-    Pg.printText()
+    page_name = input(">>")
+    # print(url)
+    page_name = page_name.replace(' ', '+')
+    # print(page_name)
+    page = http.request('GET', 'https://en.wikipedia.org/wiki/Special:Search?search='+page_name)
+    # print(page.data)
+    parser = BeautifulSoup(page.data, 'html.parser')
+    # print(parser.title.text)
+    ind = parser.title.text.find('Search results')
+    if ind == -1:
+        Pg = Page(parser)
+        Pg.printText()
+    else:
+        print('Need to search')
