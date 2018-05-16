531c327921d20276e720b691057b8a053cea7d95
Meet Parekh <meetparekh09@gmail.com>
2018-05-16 13:29:16 -0400

    Prototype version 0.0.1.3
    Description:
    	Made search class to include search results
    	implemented next results in search class
    	some nomenclature changes
    	some modular refinements

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import urllib3
 from bs4 import BeautifulSoup
-from page import *
+import page
+import search
 
 
 host_name = 'https://en.wikipedia.org/wiki/Special:Search?search='
-search_results = []
+# search_results = []
 
 urllib3.disable_warnings() #temporary fix
 http = urllib3.PoolManager()
+#
+# search_limit = 100 #default
+# offset = 0 #at the start
+# curr_offset = 0
 
 if __name__ == '__main__':
     page_name = input(">>")
     # print(url)
     page_name = page_name.replace(' ', '+')
     # print(page_name)
-    page = http.request('GET', host_name+page_name)
+    response = http.request('GET', host_name+page_name)
     # print(page.data)
-    parser = BeautifulSoup(page.data, 'html.parser')
+    parser = BeautifulSoup(response.data, 'html.parser')
     # print(parser.title.text)
     ind = parser.title.text.find('Search results')
     if ind == -1:
-        Pg = Page(parser)
+        Pg = page.Page(parser)
         Pg.printText()
     else:
-        for ind, div in enumerate(parser.find_all('div', {'class': 'mw-search-result-heading'})):
-            title = div.text
-            link = div.a['href']
-            search_results.append({'title': title, 'link': link})
-            print(str(ind)+' :: '+title)
+        # search_string = '&limit='+str(search_limit)+'&offset='+str(offset)
+        # page = http.request('GET', host_name+page_name+search_string)
+        # parser = BeautifulSoup(page.data, 'html.parser')
+        #
+        # for ind, div in enumerate(parser.find_all('div', {'class': 'mw-search-result-heading'})):
+        #     title = div.text
+        #     link = div.a['href']
+        #     search_results.append({'title': title, 'link': link})
+        #     print(str(ind)+' :: '+title)
+        SearchObject = search.Search(page_name)
+        SearchObject.display_next()
