f56f411be2f3ac63b09a1842ed515649e42eaefa
Meet Parekh <meetparekh@Meets-MacBook-Pro.local>
2018-05-16 13:29:16 -0400

    Prototype version 0.0.1.3
    Description:
    	Made search class to include search results
    	implemented next results in search class
    	some nomenclature changes
    	some modular refinements

--- page.py
+++ page.py
@@ -1,15 +1,16 @@
 import urllib3
 from bs4 import BeautifulSoup
-from main import http
+import main
 
 class Page:
     """Holds data section for wikipedia page."""
     def __init__(self, page):
         if isinstance(page, str):
-            page = http.request('GET', page)
-            self.parser = BeautifulSoup(page.data, 'html.parser')
+            response = main.http.request('GET', main.host_name+page)
+            self.parser = BeautifulSoup(response.data, 'html.parser')
         else:
             self.parser = page
 
     def printText(self):
         print(self.parser.text)
+        
