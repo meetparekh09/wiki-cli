967964c238c1c31bcade067c5a260b8a81146081
Meet Parekh <meetparekh09@gmail.com>
2018-05-16 14:39:38 -0400

    Prototype version 0.0.1.5
    Description:
    	Completed the feature of select within search, now selects ind outside of search results as well, also error checking done to ensure that search index that does not exist is not fetched

--- main.py
+++ main.py
@@ -6,45 +6,21 @@ import search
 
 host_name = 'https://en.wikipedia.org'
 special_search_strt = '/wiki/Special:Search?search='
-# search_results = []
 
-urllib3.disable_warnings() #temporary fix
+urllib3.disable_warnings()
 http = urllib3.PoolManager()
-#
-# search_limit = 100 #default
-# offset = 0 #at the start
-# curr_offset = 0
+
 
 if __name__ == '__main__':
-    page_name = input(">>")
-    # print(url)
+    page_name = input("wikipedia.org$ ")
     page_name = page_name.replace(' ', '+')
-    # print(page_name)
     response = http.request('GET', host_name+special_search_strt+page_name)
-    # print(page.data)
     parser = BeautifulSoup(response.data, 'html.parser')
-    # print(parser.title.text)
+
     ind = parser.title.text.find('Search results')
     if ind == -1:
         Pg = page.Page(parser)
         Pg.printText()
     else:
-        # search_string = '&limit='+str(search_limit)+'&offset='+str(offset)
-        # page = http.request('GET', host_name+page_name+search_string)
-        # parser = BeautifulSoup(page.data, 'html.parser')
-        #
-        # for ind, div in enumerate(parser.find_all('div', {'class': 'mw-search-result-heading'})):
-        #     title = div.text
-        #     link = div.a['href']
-        #     search_results.append({'title': title, 'link': link})
-        #     print(str(ind)+' :: '+title)
         SearchObject = search.Search(page_name)
-        # for i in range(20):
-            # SearchObject.display_next()
-        # SearchObject.display_prev()
-        # SearchObject.display_prev()
         SearchObject.display_next()
-        # SearchObject.display_prev()
-        # SearchObject.display_next()
-        Pg = SearchObject.select(3)
-        Pg.printText()
